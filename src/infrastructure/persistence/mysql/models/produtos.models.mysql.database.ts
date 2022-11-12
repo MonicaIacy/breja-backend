6a9248a8da925b5d7506b6349684d9eb99bc22a9
MonicaIacy <monicamendes81@yahoo.com>
2022-11-12 17:27:56 -0300

    fix: solving migration bug

---
+++ src/infrastructure/persistence/mysql/models/produtos.models.mysql.database.ts
@@ -0,0 +1,19 @@
+import { MysqlDatabase } from "../mysql.database"
+import * as Sequelize from "sequelize"
+
+export default MysqlDatabase.getInstance().createModel("products", {
+  productId: {
+    type: Sequelize.DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: Sequelize.DataTypes.STRING,
+  description: Sequelize.DataTypes.STRING,
+  style: Sequelize.DataTypes.STRING,
+  ibu: Sequelize.DataTypes.INTEGER,
+  color: Sequelize.DataTypes.STRING,
+  teor: Sequelize.DataTypes.FLOAT,
+  temperature: Sequelize.DataTypes.STRING,
+  price: Sequelize.DataTypes.FLOAT,
+  inventory: Sequelize.DataTypes.BOOLEAN,
+})
