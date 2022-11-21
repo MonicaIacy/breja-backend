072c18e9f1e149f15b31f82ec637cf2f864d4038
MonicaIacy <monicamendes81@yahoo.com>
2022-11-21 20:22:02 -0300

    fix: jest tests

--- src/domain/usecases/addresses/find.cep.usecase.spec.ts
+++ src/domain/usecases/addresses/find.cep.usecase.spec.ts
@@ -1,24 +1,18 @@
-import findCepUseCase from './find.cep.usecase'
+import findCepUseCase from "./find.cep.usecase"
 
 test(" Teste unitário de findCepUsecase", async () => {
-    const cep: any = {
-        cep: 13386060
-    
-    }
-    await findCepUseCase.execute(cep)
-    const cep2: object = {
-        cep: '13386060',
-        endereco:{
-          cep: '13386-060',
-          logradouro: 'Rua João Severiano da Silva',
-          complemento: "",
-          bairro: "Jardim Santa Rita I",
-          cidade: "Nova Odessa",
-          estado: "SP"
-        },
-       
-    }
+  const cep: any = {
+    cep: 13386060,
+  }
+  await findCepUseCase.execute(cep)
+  const cep2: object = {
+      cep: "13386-060",
+      logradouro: "Rua João Severiano da Silva",
+      complemento: "",
+      bairro: "Jardim Santa Rita I",
+      cidade: "Nova Odessa",
+      estado: "SP",
+  }
 
-
-    expect(await findCepUseCase.execute({cep:'13386060'})).toMatchObject(cep2)
+  expect(await findCepUseCase.execute(cep)).toMatchObject(cep2)
 })
