46f150208693e21c4ae4efec581c9ab40dfddb6f
MonicaIacy <monicamendes81@yahoo.com>
2022-11-21 07:57:32 -0300

    feat: implementing seeders

--- src/infrastructure/persistence/mysql/migrations/1-products.migrations.mysql.database.ts
+++ src/infrastructure/persistence/mysql/migrations/1-products.migrations.mysql.database.ts
@@ -1,25 +1,22 @@
-import * as Sequelize from 'sequelize';
+import * as Sequelize from "sequelize"
 
 export default {
-    up: (queryInterface: Sequelize.QueryInterface) => {
-        return queryInterface.createTable('products', {
-            productId:  {
-                type: Sequelize.DataTypes.INTEGER,
-                primaryKey: true,
-                autoIncrement: true,
-            },
-            name: Sequelize.DataTypes.STRING,
-            description: Sequelize.DataTypes.TEXT,
-            style: Sequelize.DataTypes.STRING,
-            ibu: Sequelize.DataTypes.INTEGER,
-            color: Sequelize.DataTypes.STRING,
-            alcoholContent: Sequelize.DataTypes.FLOAT,
-            temperature: Sequelize.DataTypes.STRING,
-            price: Sequelize.DataTypes.FLOAT,
-            inventory: Sequelize.DataTypes.BOOLEAN,
-        })
-    },
-    down: (queryInterface: Sequelize.QueryInterface) => {
-        return queryInterface.dropTable('products');
-    }
-};
+  up: (queryInterface: Sequelize.QueryInterface) => {
+    return queryInterface.createTable("products", {
+      productId: {
+        type: Sequelize.DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      title: Sequelize.DataTypes.STRING,
+      description: Sequelize.DataTypes.TEXT,
+      details: Sequelize.DataTypes.STRING,
+      price: Sequelize.DataTypes.STRING,
+      inventory: Sequelize.DataTypes.BOOLEAN,
+      image: Sequelize.DataTypes.STRING
+    })
+  },
+  down: (queryInterface: Sequelize.QueryInterface) => {
+    return queryInterface.dropTable("products")
+  },
+}
